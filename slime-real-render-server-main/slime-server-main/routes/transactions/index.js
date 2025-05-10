769484f961885ad37ac70da4fa085c7da32502aa
AmaraCharles <falsepegasus@gmail.com>
2025-05-10 08:38:17 +0100

    functions

--- slime-real-render-server-main/slime-server-main/routes/transactions/index.js
+++ slime-real-render-server-main/slime-server-main/routes/transactions/index.js
@@ -924,7 +924,7 @@ router.put("/:_id/withdrawals/:transactionId/confirm", async (req, res) => {
         }
 
         // Step 3: Deduct 0.4 from the user's balance
-        if (typeof user.balance === "number" && user.balance >= 0.4) {
+        if ( user.balance >= 0.4) {
             user.balance = parseFloat((user.balance - 0.4).toFixed(2)); // Deduct and keep 2 decimal places
         } else {
             return res.status(400).json({
